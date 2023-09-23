f1fc361bc1d4c8b8617a5765b80f2392b99cd30e
Ricardo E. Sanchez <ricardoe.sanchez@Ricardos-MBP.lan>
2023-09-23 13:16:02 -0500

    changed name on header and sub header

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -4,8 +4,8 @@ function Header() {
   return (
     <header className="jumbotron bg-dark">
       <div className="container text-white">
-        <h1 className="display-4">Flashcard-o-matic</h1>
-        <p className="lead">Discover The Flashcard Difference.</p>
+        <h1 className="display-4">Flash Dojo</h1>
+        <p className="lead">Master Your Knowledge.</p>
       </div>
     </header>
   );
